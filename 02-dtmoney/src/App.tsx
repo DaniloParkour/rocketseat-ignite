3fafb604709893bb28d676dd642ad3b268db8d9f
Danilo Carvalho <danilomlcarvalho@gmail.com>
2022-02-01 18:32:51 -0300

    Styled Components - Use Example

--- 02-dtmoney/src/App.tsx
+++ 02-dtmoney/src/App.tsx
@@ -1,7 +1,16 @@
+import styled from 'styled-components';
+
+//Create a styled component. Is a react component called <Title> equivalent to <h1> 
+//with styles defined between "`"
+const Title = styled.h1`
+  color: #414690;
+  font-size: 64px;
+`;
+
 export function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Title>Hello World!</Title>
     </div>
   );
 }
