68b2fd4580439f553e5dda8eaef6ec91f4686d3e
Danilo Carvalho <danilomlcarvalho@gmail.com>
2022-02-02 10:20:31 -0300

    Stiles some styles for Header was defined

--- 02-dtmoney/src/components/Header/styles.ts
+++ 02-dtmoney/src/components/Header/styles.ts
@@ -3,3 +3,26 @@ import styled from "styled-components";
 export const Container = styled.header`
   background: var(--blue);
 `;
+
+export const Content = styled.div`
+  max-width: 1120px;
+  margin: 0 auto; //Centralize element on horizontal
+
+  padding: 2rem 1rem 12rem;
+
+  display: flex; //Enable flex element positions
+  align-items: center; //Flex enable this configuration
+  justify-content: space-between; //Flex enable this configuration
+
+  button {
+    font-size: 1rem;
+    color: #FFF;
+    background: var(--blue-light);
+    border: 0;
+    padding: 0 2rem;
+    border-radius: 0.25rem;
+    height: 3rem;
+    
+  }
+
+`;
