dcaef98d3e1ca8e56d093a14a53cb0251ab3a492
Danilo Carvalho <danilomlcarvalho@gmail.com>
2022-02-02 11:55:52 -0300

    Component with table on Dashboard

--- 02-dtmoney/src/components/Dashboard/styles.ts
+++ 02-dtmoney/src/components/Dashboard/styles.ts
@@ -2,8 +2,5 @@ import styled from "styled-components";
 
 export const Container = styled.main`
   max-width: 1120;
-  //display: flex;
-  //justify-content: center;
-  //margin: 0 auto;
   padding: 2.5rem 1rem; // (2.5 TOP, DOWN), (1 LEFT, RIGHT)
 `;
---
+++ 02-dtmoney/src/components/TransactionsTable/index.tsx
@@ -0,0 +1,45 @@
+import { Contanier } from "./styles";
+
+export function TransactionsTable() {
+  return(
+    <Contanier>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td>R$12.000,00</td>
+            <td>Desenvolvimento</td>
+            <td>10/08/2021</td>
+          </tr>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td>R$12.000,00</td>
+            <td>Desenvolvimento</td>
+            <td>10/08/2021</td>
+          </tr>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td>R$12.000,00</td>
+            <td>Desenvolvimento</td>
+            <td>10/08/2021</td>
+          </tr>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td>R$12.000,00</td>
+            <td>Desenvolvimento</td>
+            <td>10/08/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Contanier>
+  );
+}
