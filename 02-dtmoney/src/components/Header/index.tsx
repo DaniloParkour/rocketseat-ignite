ba5572b6954748081cc997ae12654ef3719f49d3
Danilo Carvalho <danilomlcarvalho@gmail.com>
2022-02-02 18:05:53 -0300

    Modal with React-Modal
    yarn add react-modal
    yarn add @types/react-modal -D
    Define isOpen and onRequestClose on <Modal />

--- 02-dtmoney/src/components/Header/index.tsx
+++ 02-dtmoney/src/components/Header/index.tsx
@@ -1,14 +1,31 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
 export function Header() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  //handle for functions that is called by any user interaction to UI
+  function handleSetOpenNewTransactionModal(value: boolean) {
+    setIsNewTransactionModalOpen(value);
+  }
+
   return(
     <Container>
       <Content>
         <img src={logoImg} alt="dt money"/>
-        <button type="button">
+        <button type="button" onClick={() => handleSetOpenNewTransactionModal(true)}>
           Nova Transação
         </button>
+
+        <Modal
+          isOpen={isNewTransactionModalOpen}
+          onRequestClose={() => setIsNewTransactionModalOpen(false)}
+        >
+          <h2>Cadastrar Transação</h2>
+        </Modal>
       </Content>
     </Container>
   )
