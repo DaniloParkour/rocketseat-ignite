6b5e86c720c6a5e9e19d337fd2025152c66eeede
Danilo Carvalho <danilomlcarvalho@gmail.com>
2022-02-02 13:17:42 -0300

    MigageJS configure a server and tetch an endpoint

--- 02-dtmoney/src/index.tsx
+++ 02-dtmoney/src/index.tsx
@@ -2,6 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
+import { createServer } from 'miragejs';
+
+//Create server FakeAPI
+createServer({
+  routes() {
+    this.namespace = 'api'; //All routes after "/api" is for MirageJS
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Selled Item',
+          createdAt: new Date()
+        }
+      ]
+    });
+
+  }
+})
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
