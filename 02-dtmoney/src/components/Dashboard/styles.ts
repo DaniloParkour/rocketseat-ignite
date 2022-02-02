71338458d390272730f84b8f29b6bd1ea9e1453b
Danilo Carvalho <danilomlcarvalho@gmail.com>
2022-02-02 11:36:01 -0300

    Styling more components

--- 02-dtmoney/src/components/Dashboard/styles.ts
+++ 02-dtmoney/src/components/Dashboard/styles.ts
@@ -2,9 +2,8 @@ import styled from "styled-components";
 
 export const Container = styled.main`
   max-width: 1120;
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  margin: 0 auto;
+  //display: flex;
+  //justify-content: center;
+  //margin: 0 auto;
   padding: 2.5rem 1rem; // (2.5 TOP, DOWN), (1 LEFT, RIGHT)
 `;
