bdb6a26938239e48de06fb2accb807c29d756717
Danilo Carvalho <danilomlcarvalho@gmail.com>
2022-02-03 10:49:41 -0300

    Add inital value to database of miragejs

--- 02-dtmoney/src/index.tsx
+++ 02-dtmoney/src/index.tsx
@@ -12,6 +12,32 @@ createServer({
     transaction: Model
   },
 
+  //Load a initial data on table transactions do database
+  //Transactions is the table that is the plural name of model
+  seeds(server) {
+    server.db.loadData({
+      //OBS: Is a Table name (model in plural)
+      transaction: [
+        {
+          id: 1, //ever has id numeric and autoincrement
+          title: "Freelance Website",
+          type: "deposit",
+          category: "Development",
+          amount: 6000,
+          createdAt: new Date("2021-10-02 09:00:00")
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "deposit",
+          category: "Casa",
+          amount: 800,
+          createdAt: new Date("2021-10-12 09:32:12")
+        }
+      ]
+    });
+  },
+
   routes() {
     this.namespace = 'api'; //All routes after "/api" is for MirageJS
 
