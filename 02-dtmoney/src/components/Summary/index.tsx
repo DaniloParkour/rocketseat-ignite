55d04060e4e47dc16a569eecc638e3ab6f038975
Danilo Carvalho <danilomlcarvalho@gmail.com>
2022-02-03 18:56:23 -0300

    INTL format currency BRL

--- 02-dtmoney/src/components/Summary/index.tsx
+++ 02-dtmoney/src/components/Summary/index.tsx
@@ -33,21 +33,42 @@ export function Summary() {
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas" />
         </header>
-        <strong>{"R$"+summary.deposits}</strong>
+        <strong>
+          {
+            new Intl.NumberFormat("pt-BR", {
+              style: "currency",
+              currency: "BRL"
+            }).format(summary.deposits)
+          }
+        </strong>
       </div>
       <div>
         <header>
           <p>Saídas</p>
           <img src={outcomeImg} alt="Saídas" />
         </header>
-        <strong>{"- R$"+summary.withdraws}</strong>
+        <strong>
+          {
+            new Intl.NumberFormat("pt-BR", {
+              style: "currency",
+              currency: "BRL"
+            }).format(-summary.withdraws)
+          }
+        </strong>
       </div>
       <div className='highlight-background'>
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Total" />
         </header>
-        <strong>{"R$"+summary.total}</strong>
+        <strong>
+          {
+            new Intl.NumberFormat("pt-BR", {
+              style: "currency",
+              currency: "BRL"
+            }).format(summary.total)
+          }
+        </strong>
       </div>
     </Container>
   );
