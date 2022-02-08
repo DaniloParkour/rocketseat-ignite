59a9d3a8d0d73249502640f397aad142c786c46a
Danilo Carvalho <danilomlcarvalho@gmail.com>
2022-02-08 13:10:00 -0300

    Alterando o <title> do <Head> de forma dinâmica

--- 03-ignews/src/pages/index.tsx
+++ 03-ignews/src/pages/index.tsx
@@ -1,7 +1,17 @@
+//Tudo que for colocado no Head importado aqui será anexado ao head do _document.ts
+//Podemos alterar os valores do _document enquanto a aplicação está executando
+//Podemos usar também para SEO, entre outros ex: <meta>.
+import Head from 'next/head'
+
 import styles from '../../styles/home.module.scss'
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Hello <span>W</span>orld</h1>
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1 className={styles.title}>Hello <span>W</span>orld</h1>
+    </>
   )
 }
