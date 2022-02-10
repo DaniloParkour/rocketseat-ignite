97f0260284a5c5725126ea073cb5b61ae9267d2f
Danilo Carvalho <danilomlcarvalho@gmail.com>
2022-02-10 19:57:12 -0300

    Login e save user no FaunaDB iniciado

--- 03-ignews/src/pages/api/auth/[...nextauth].ts
+++ 03-ignews/src/pages/api/auth/[...nextauth].ts
@@ -2,29 +2,29 @@ import NextAuth from 'next-auth'
 // import Providers from 'next-auth/providers'
 import GitHubProvider from 'next-auth/providers/github'
 
+import { query as q } from 'faunadb'
+import { fauna } from  '../../../services/fauna'
+
 export default NextAuth ({
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       //Escopo é para definir quais informações queremos ter acesso
-      scope: 'read:user' //'read:user, outro:escopo, ...'
+      // scope: 'read:user' //'read:user, outro:escopo, ...'
     }),
-  ]
+  ],
+  callbacks: {
+    async signIn({user, account, profile, email}) {
+
+      await fauna.query(
+        q.Create(
+          q.Collection('users'),
+          { data: {email: user.email} }
+        )
+      )
+
+      return true
+    },
+  }
 })
-
-/*
-
-import GitHubProvider from 'next-auth/providers/github'
-
-export default NextAuth ({
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET
-    }),
-  ]
-})
-
-
-*/
