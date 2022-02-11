e71c8e3076db074c956659c96a253dd509b7ead7
daniloparkour <danilomlcarvalho@gmail.com>
2022-02-11 13:24:30 -0300

    Usuário customer sendo adicionado ao Fauna o ID do Stripe para não duplicar curstomer Stripe quando for pagar novamente

--- 03-ignews/src/pages/api/subscribe.ts
+++ 03-ignews/src/pages/api/subscribe.ts
@@ -1,5 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
+import { fauna } from "../../services/fauna";
+import { query as q } from 'faunadb'
 import { stripe } from "../../services/stripe";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -9,14 +11,43 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     //Pagar informações do usuário que comprou a assinatura
     const session = await getSession({ req }) //token, user, data de exppiração
 
-    //Cadastrar user no Stripe
-    const stripeCustomer = await stripe.customers.create({
-      email: session.user.email,
-      // metadata
-    })
+    const user = await fauna.query(
+      q.Get(
+        q.Match(
+          q.Index('user_by_email'),
+          q.Casefold(session.user.email)
+        )
+      )
+    )
+
+    let customerId = user.data.stripe_customer_id
+
+    if(!customerId) { //Se existe o usuário com esse email no Fauna mas não tem um "stripe_customer_id" cria novo customer no stripe
+
+      //Cadastrar user no Stripe
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email,
+        // metadata
+      })
+
+      //Salvar no FaunaDB usuário Stripe criado para reaproveitar quando for acessado novamente
+      await fauna.query(
+        q.Update(
+          q.Ref( q.Collection('users'), user.ref.id ),
+          {
+            data: {
+              stripe_customer_id: stripeCustomer.id
+            }
+          }
+        )
+      )
+
+      customerId = stripeCustomer.id
+
+    }
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create ({
-      customer: stripeCustomer.id, //Informar qual customer fez o checkout
+      customer: customerId, //Informar qual customer fez o checkout
       payment_method_types: ['card'],
       billing_address_collection: 'required', //Nesse caso seria melhor "auto" esta sendo usado "required" para ver mais a frente ma funcionalidade
       line_items: [
