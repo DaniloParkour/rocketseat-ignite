be136a68814a7e4de5d52507a7dbee1e58d94e94
daniloparkour <danilomlcarvalho@gmail.com>
2022-02-11 10:56:49 -0300

    Ajustes no login e save no FaunaDB

--- 03-ignews/src/pages/api/auth/[...nextauth].ts
+++ 03-ignews/src/pages/api/auth/[...nextauth].ts
@@ -17,14 +17,35 @@ export default NextAuth ({
   callbacks: {
     async signIn({user, account, profile, email}) {
 
-      await fauna.query(
-        q.Create(
-          q.Collection('users'),
-          { data: {email: user.email} }
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'), //Compara o Index('user_by_email') com o user.email em minúsculo
+                  q.Casefold(user.email) //user.email em LowerCase
+                )
+              )
+            ),
+            q.Create( //Se Não Existe algum Match (user_by_email com user.email) => Cria o novo na collection usuários
+              q.Collection('users'),
+              { data: {email: user.email} }
+            ),
+            q.Get( //Se Não (se existe esse Match) => Pega o usuário  da collection
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
         )
-      )
+  
+        return true
+      } catch {
+        return false
+      }
 
-      return true
     },
   }
 })
